9dd3a9377d21bc33fd8257a25a28a57887c38464
Student <example@example.com>
2019-06-14 15:45:08 -0700

    add functions and start joingin them to the array

--- js/scripts.js
+++ js/scripts.js
@@ -8,60 +8,26 @@ $(document).ready(function () {
     var array = []
     var lowEnd = 0;
     var highEnd = (inputNum);
-    // var array = (i = lowEnd i <= highEnd; i++)
-    console.log(lowEnd)
-    console.log(highEnd)
     for (var i = lowEnd; i <= highEnd; i++) {
       array.push(i)
     };
     console.log(array)
 
-      function array(i) {
-        if (i === 3) {
-          return "I'm sorry dave"
+      array.forEach(function(number) {
+        if number ===3
+         return "I'm sorry dave"
         }
-        else if (i === 2) {
+        else if (array === 2) {
           return "boop"
         }
-        else if (i === 1) {
+        else if (array === 1) {
           return "beep"
         }
-      };
+      });
+
 
 
       var output = array
       $(".result").text(output);
       // take number from array and convert to fianl state
 });
-});
-
-
-
-
-    // var array = [lowEnd, highEnd];
-    //         console.log(array);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- //
- // //closes form
- // //closes doc read
