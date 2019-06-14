6587672d56e32076e138089df714a91fe56f4af9
Student <example@example.com>
2019-06-14 12:45:29 -0700

    take in numbers and produces array of all between '0' and user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,60 @@
 $(document).ready(function () {
 
-  var numTwoChange = ["1", "2", "3"];
+
+  var numToChange = ["1", "2", "3"];
 
   $('#beginbutton').click(function (event) {
+
     var message = ($('input#original').val()).split(' ');
+
     console.log(message);
-    var list = [];
-    for (var i = lowEnd; i <= highEnd; i++) {
-      list.push(i);
-      console.log(list)
-    }
-    for (index = 0; index < message.length; index++) {
-      if (numTwoChange.includes(message[index])) {
-        message.splice(index, 1, "-")
-      };
-    };
-    var output = message.join("")
-    $(".result").text(output);
+
+  var list = [];
+  var lowEnd = 0;
+  console.log(lowEnd)
+  var highEnd = (message)
+  console.log(highEnd)
+  for (var i = lowEnd; i <= highEnd; i++) {
+
+    list.push(i);
+    console.log(list)
     event.preventDefault();
-  }); //closes form
-}); //closes doc read
-if (number) === "3"(true) {
-(.replace"I'm sorry dave")
-};
-else if number === "2"(true) {
-.replace"boop";
-}
-else if number.includes "1"(true) {
-  .replace"beep";
-}
-else {
-  return "input"
-}
+
+    //       if (numTwoChange.includes(message[index])) {
+    //
+    //         message.splice(index, 1, "-")
+    //
+    //       };
+    //
+    //     };
+    //
+    //     var output = message.join("")
+    //
+    //     $(".result").text(output);
+    //
+    //
+    // take number from array and convert to fianl state
+    //
+    if (list) "3"(true)
+      (replace)
+      "I'm sorry dave";
+
+    elseif (list)"2"(true)
+      (replace) "boop";
+
+    else if number.includes "1"(true) {
+      .replace"beep";
+    }
+    else {
+      return "input"
+    }
+
+  };
+});
+});
+//
+//     for (index = 0; index < message.length; index++) {
+//
+//
+// }); //closes form
+// }); //closes doc read
