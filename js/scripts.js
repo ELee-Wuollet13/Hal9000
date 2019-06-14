50b69bcb1ca0f31ce9acf9e0392e0ecc8a2600a0
Student <example@example.com>
2019-06-14 09:34:22 -0700

    add number intake and HTML

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".form").submit(function(event) {
+    event.preventDefault();
+    var basicNumber = $("#input1").val();
+    console.log(basicNumber)
+    var allTheNumbers = basicNumber(0 =< basicNumber[])
+    var arrayedNumber = basicNumber.split(" ");
+    arrayedNumber.reverse();
+    // call outside functions
+  });
+});
