bb86ef6997731fe595110ea35cee4e0630cd278f
Student <example@example.com>
2019-06-14 11:33:34 -0700

    add 'if/then' statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,34 @@
-$(document).ready(function(){
-  $(".form").submit(function(event) {
+$(document).ready(function () {
+
+  var numTwoChange = ["1", "2", "3"];
+
+  $('#beginbutton').click(function (event) {
+    var message = ($('input#original').val()).split(' ');
+    console.log(message);
+    var list = [];
+    for (var i = lowEnd; i <= highEnd; i++) {
+      list.push(i);
+      console.log(list)
+    }
+    for (index = 0; index < message.length; index++) {
+      if (numTwoChange.includes(message[index])) {
+        message.splice(index, 1, "-")
+      };
+    };
+    var output = message.join("")
+    $(".result").text(output);
     event.preventDefault();
-    var basicNumber = $("#input1").val();
-    console.log(basicNumber)
-    var allTheNumbers = basicNumber(0 =< basicNumber[])
-    var arrayedNumber = basicNumber.split(" ");
-    arrayedNumber.reverse();
-    // call outside functions
-  });
-});
+  }); //closes form
+}); //closes doc read
+if (number) === "3"(true) {
+(.replace"I'm sorry dave")
+};
+else if number === "2"(true) {
+.replace"boop";
+}
+else if number.includes "1"(true) {
+  .replace"beep";
+}
+else {
+  return "input"
+}
