1cf5ea23e5ac53b86f622feda6ec98b8769e9422
Student <example@example.com>
2019-06-14 14:45:04 -0700

    provide a funcitonal array of all numbers between 0 and user input

--- js/scripts.js
+++ js/scripts.js
@@ -2,40 +2,66 @@ $(document).ready(function () {
   $('#beginbutton').click(function (event) {
     event.preventDefault();
     // var numToChange = ["1", "2", "3"];
-    var input = parseInt($('input#original').val());
-    console.log(input);
+    var inputNum = parseInt($('input#original').val());
+    console.log(inputNum);
 
-    var list = [];
+    var array = []
     var lowEnd = 0;
+    var highEnd = (inputNum);
+    // var array = (i = lowEnd i <= highEnd; i++)
     console.log(lowEnd)
-    var highEnd = (input)
     console.log(highEnd)
     for (var i = lowEnd; i <= highEnd; i++) {
-      list.push(i);
-      console.log(list)
-            //
-            // if (numToChange.includes(message[index])) {
-            //   message.splice(index, 1, "-")
-            // };
-          };
-          function list(i) {
+      array.push(i)
+    };
+    console.log(array)
 
-            if (i === 3) {
-              return "I'm sorry dave"
-            }
-            else if (i ===2) {
-              return "boop";
-            }
-          }
+      function array(i) {
+        if (i === 3) {
+          return "I'm sorry dave"
+        }
+        else if (i === 2) {
+          return "boop"
+        }
+        else if (i === 1) {
+          return "beep"
+        }
+      };
 
 
-          var output = list.join("")
-         $(".result").text(output);
+      var output = array
+      $(".result").text(output);
       // take number from array and convert to fianl state
+});
+});
 
 
-    });
-  });
 
- //closes form
- //closes doc read
+
+    // var array = [lowEnd, highEnd];
+    //         console.log(array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //
+ // //closes form
+ // //closes doc read
