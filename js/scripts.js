138f641f1479ac21fb612e25b2a44b7a4f9b64e0
Student <example@example.com>
2019-06-14 16:30:54 -0700

    change array/function relationship. still not working

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     var inputNum = parseInt($('input#original').val());
     console.log(inputNum);
 
-    var array = []
+    var array = [1, 3, 5]
     var lowEnd = 0;
     var highEnd = (inputNum);
     for (var i = lowEnd; i <= highEnd; i++) {
@@ -13,21 +13,20 @@ $(document).ready(function () {
     };
     console.log(array)
 
-      array.forEach(function(number) {
-        if number ===3
-         return "I'm sorry dave"
+    array.forEach(function(number) {
+      if (number = 3) {
+        $("I'm sorry dave").show();
+        if (number = 2) {
+          $("boop").show();
+        if (number = 1) {
+          $("beep").show();
         }
-        else if (array === 2) {
-          return "boop"
         }
-        else if (array === 1) {
-          return "beep"
-        }
-      });
+      }
+    })
+  });
 
-
-
-      var output = array
-      $(".result").text(output);
-      // take number from array and convert to fianl state
+  var output = array
+  $(".result").text(output);
+  // take number from array and convert to fianl state
 });
